0ea3645d872c32afa84bae4f5d7351363fa1433a
p3732 <p3732@users.noreply.github.com>
2019-05-09 06:05:30 +0200

    update numba script

--- numba_device_function_loop_unrolling.py
+++ numba_device_function_loop_unrolling.py
@@ -24,13 +24,9 @@ def rdist_fixed(array_a, array_b):
     
     return result
 
-
 @cuda.jit
-def test_loop_unrolling(
-    array_a, array_b, dists, use_dev_fun
-):
+def init_arrays(array_a, array_b):
     n_elements = array_a.shape[0]
-    n_loops = 1000
     
     # elements handled by this thread
     thread_id = cuda.grid(1)
@@ -38,18 +34,38 @@ def test_loop_unrolling(
     thread_size = int(ceil(n_elements / n_threads))
     start_pos = thread_id * thread_size
     end_pos = min((thread_id + 1) * thread_size, n_elements)
-    
+
     # put some data in arrays
     for i in range(start_pos, end_pos):
         for j in range(N_DIMENSIONS):
             array_a[i][j] = (thread_id * i * j) % (n_elements * N_DIMENSIONS)
             array_b[i][j] = (thread_id * i + j) % (n_elements * N_DIMENSIONS)
     
-    # run either rdist with or without device function distances
+
+@cuda.jit
+def test_loop_unrolling(
+    array_a, array_b, dists, use_dev_fun, use_fixed
+):
+    n_elements = array_a.shape[0]
+    n_loops = 1000
+    # elements handled by this thread
+    thread_id = cuda.grid(1)
+    n_threads = cuda.gridsize(1)
+    thread_size = int(ceil(n_elements / n_threads))
+    start_pos = thread_id * thread_size
+    end_pos = min((thread_id + 1) * thread_size, n_elements)
+
+    # run rdist either with or without device function
     if use_dev_fun:
-        for epoch in range(n_loops):
-            for i in range(start_pos, end_pos):
-                dists[i] = rdist_fixed(array_a[i], array_b[i])
+        if use_fixed:
+            for epoch in range(n_loops):
+                for i in range(start_pos, end_pos):
+                    dists[i] = rdist_fixed(array_a[i], array_b[i])
+        else:
+            for epoch in range(n_loops):
+                for i in range(start_pos, end_pos):
+                    dists[i] = rdist(array_a[i], array_b[i])
+        
     else:
         for epoch in range(n_loops):
             for i in range(start_pos, end_pos):
@@ -67,48 +83,46 @@ def test():
     
     global N_DIMENSIONS
     N_DIMENSIONS = 2
-    n_elements = 10000
+    n_elements = 100000
     
-    array_a = np.zeros((n_elements, N_DIMENSIONS), dtype=np.float32)
-    array_b = np.zeros((n_elements, N_DIMENSIONS), dtype=np.float32)
-    dists_dev_fun = np.zeros(n_elements, dtype=np.float32)
-    dists_non_dev_fun = np.zeros(n_elements, dtype=np.float32)
-    
-    # copy arrays to device
-    d_array_a = cuda.to_device(array_a)
-    d_array_b = cuda.to_device(array_b)
-    
-    d_dists_dev_fun = cuda.to_device(dists_dev_fun)
-    d_dists_non_dev_fun = cuda.to_device(dists_non_dev_fun)
+    d_array_a = cuda.device_array((n_elements, N_DIMENSIONS), dtype=np.float32)
+    d_array_b = cuda.device_array((n_elements, N_DIMENSIONS), dtype=np.float32)
+    d_dists = cuda.device_array(n_elements, dtype=np.float32)    
     
     # run on gpu
-    print("starting kernels with {} threads in {} blocks.", n_threads, n_blocks)
+    print("starting kernels with {} threads in {} blocks.".format(n_threads, n_blocks))
+    init_arrays[n_blocks, threads_per_block](d_array_a, d_array_b)
     
-    # dummy kernel run to get compilation out of the way
-    start=time.time()
+    # dummy kernel run to get compilation out of time measurement
     test_loop_unrolling[n_blocks, threads_per_block](
-        d_array_a, d_array_b, d_dists_dev_fun, True
+        d_array_a, d_array_b, d_dists, True, True
     )
-    end=time.time()
-    print("dummy run completed in {}", end-start)
     
     start=time.time()
     test_loop_unrolling[n_blocks, threads_per_block](
-        d_array_a, d_array_b, d_dists_dev_fun, True
+        d_array_a, d_array_b, d_dists, True, True
     )
     end=time.time()
-    print("kernel using device function completed in {}", end-start)
+    print("kernel using device function with fixed iterator completed in {:.6f}".format(end-start))
+    dists_dev_fun_fixed = d_dists.copy_to_host()
+
+    start=time.time()
+    test_loop_unrolling[n_blocks, threads_per_block](
+        d_array_a, d_array_b, d_dists, True, False
+    )
+    end=time.time()
+    print("kernel using device function without fixed iterator completed in {:.6f}".format(end-start))
+    dists_dev_fun_non_fixed = d_dists.copy_to_host()
     
     start=time.time()
     test_loop_unrolling[n_blocks, threads_per_block](
-        d_array_a, d_array_b, d_dists_non_dev_fun, False
+        d_array_a, d_array_b, d_dists, False, False
     )
     end=time.time()
-    print("kernel without device function completed in {}", end-start)
+    print("kernel without device function completed in {:.6f}".format(end-start))
+    dists_non_dev_fun = d_dists.copy_to_host()
     
     # copy result back from device
-    dists_dev_fun = d_dists_dev_fun.copy_to_host()
-    dists_non_dev_fun = d_dists_non_dev_fun.copy_to_host()
     
-    return dists_dev_fun, dists_non_dev_fun
+    return (dists_dev_fun_fixed, dists_dev_fun_non_fixed, dists_non_dev_fun)
 
